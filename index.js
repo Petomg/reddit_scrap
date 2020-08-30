2991a4f026d4162b9fc17c3e936414b18ad8999e
Jesús Mañez <yisus@localhost.localdomain>
2020-08-30 00:43:54 -0300

    Dynamic link collection (ish)

--- index.js
+++ index.js
@@ -4,29 +4,31 @@ const REDDIT_POST_REF = '/r/PublicFreakout/comments/ii3hg7/trump_doll_put_in_the
 
 (async () => {
     /* Initiate the Puppeteer browser */
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     const page2 = await browser.newPage();
 
     /* Go to the reddit post page and wait for it lo load */
-    console.log(REDDIT_URL(REDDIT_POST_REF));
-    await page.goto(REDDIT_URL(REDDIT_POST_REF), { waitUntil: 'networkidle0' });
+    await page.goto('https://www.reddit.com', { waitUntil: 'networkidle0' });
 
-    /* Run javascript inside of the page */
-    let data = await page.evaluate(() => {
-        let title = document.querySelector('h1[class="_eYtD2XCVieq6emjKBH3m"]').innerText;
-        let video_src = document.querySelector('video').getAttribute("src");
-
-        return {
-            title,
-            video_src,
+    let links = [];
+    let elements = await page.$$('div._1poyrkZ7g36PawDueRza-J');
+    console.log(elements);
+    for (let element of elements) {
+            
+        let isPromo = await element.$('span._2oEYZXchPfHwcf9mTMGMg8');
+        let isVideo = await element.$('video');
+        console.log(isPromo === null && isVideo !== null);
+        if(isPromo === null && isVideo !== null){
+            let ref = await element.$eval('a.SQnoC3ObvgnGjWt90zD9Z', el => el.getAttribute('href'));
+            links.push(REDDIT_URL(ref));
         }
-    });
+    }    
 
-    let links = ["https://www.reddit.com/r/ActualPublicFreakouts/comments/ii2cy1/blm_activists_physically_assault_gay_man_and_call/","https://www.reddit.com/r/PublicFreakout/comments/ii3hg7/trump_doll_put_in_the_guillotine_outside_the/","https://www.reddit.com/r/nextfuckinglevel/comments/ii55mv/marlon_brando_rejecting_the_oscar_for_the/"];
     let count = 0;
-    console.log(data);
-    async function bajar_archivo(link) { 
+    console.log(links);
+
+   /* async function bajar_archivo(link) { 
       
         console.log(link);
       
@@ -58,6 +60,6 @@ const REDDIT_POST_REF = '/r/PublicFreakout/comments/ii3hg7/trump_doll_put_in_the
     }
     bajar_archivo(links[count]);
   
-    
+  */
 })();
 
